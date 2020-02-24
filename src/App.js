2b43e52feaa06c3e75b92bc2ac46a412079eef10
Alexander Ottenhoff <alexander.ottenhoff@gmail.com>
2020-02-24 12:51:58 +0100

    Created router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react";
-import { Container, Card, CardContent, Typography } from "@material-ui/core";
-import {
-  FirebaseDatabaseProvider,
-  FirebaseDatabaseNode
-} from "@react-firebase/database";
-import firebase from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
+import { Switch, Route, Redirect } from "react-router";
+import Login from "./Login";
+import { BrowserRouter } from "react-router-dom";
 
-const App = () => {
-  return (
-    <FirebaseDatabaseProvider {...firebaseConfig} firebase={firebase}>
-      <Container maxWidth="xs">
-        <FirebaseDatabaseNode path="18IbXgkejpKHOdUiMRpvLliMPIqnsBF3_gJH-AHH9ZA8/User">
-          {({ value: users }) =>
-            users &&
-            users.map(
-              user =>
-                user && (
-                  <Card key={user.id}>
-                    <CardContent>
-                      <Typography>{JSON.stringify(user, null, 2)}</Typography>
-                    </CardContent>
-                  </Card>
-                )
-            )
-          }
-        </FirebaseDatabaseNode>
-      </Container>
-    </FirebaseDatabaseProvider>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Redirect to="/login" />
+    </Switch>
+  </BrowserRouter>
+);
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Card, CardContent, Typography } from "@material-ui/core";
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseNode,
+} from "@react-firebase/database";
+import firebase from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
+import { Router } from "react-router";
+
+const Login = () => (
+  <FirebaseDatabaseProvider {...firebaseConfig} firebase={firebase}>
+    <Container maxWidth="xs">
+      <FirebaseDatabaseNode path="18IbXgkejpKHOdUiMRpvLliMPIqnsBF3_gJH-AHH9ZA8/User">
+        {({ value: users }) =>
+          users &&
+          users.map(
+            user =>
+              user && (
+                <Card key={user.id}>
+                  <CardContent>
+                    <Typography>{JSON.stringify(user, null, 2)}</Typography>
+                  </CardContent>
+                </Card>
+              )
+          )
+        }
+      </FirebaseDatabaseNode>
+    </Container>
+  </FirebaseDatabaseProvider>
+);
+
+export default Login;
